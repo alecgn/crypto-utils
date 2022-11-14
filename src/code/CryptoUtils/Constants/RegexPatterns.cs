0ce7e6528bc632374073d5c0c9aa8302869e8bd0
Alessandro Cagliostro <alecgn@hotmail.com>
2022-11-13 23:21:24 -0300

    improved regexes for base64 and hexadecimal strings

--- src/code/CryptoUtils/Constants/RegexPatterns.cs
+++ src/code/CryptoUtils/Constants/RegexPatterns.cs
@@ -1,9 +1,9 @@
 ﻿namespace CryptoUtils.Constants
 {
-	internal static class RegexPatterns
-	{
-		internal const string Base64String = @"^[a-zA-Z0-9+\/=]{4,}$";
+    internal static class RegexPatterns
+    {
+        internal const string Base64String = @"^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$";
 
-		internal const string HexadecimalString = @"^(0(?:x|X))?([0-9a-fA-F]{2,})$";
-	}
+        internal const string HexadecimalString = @"^(?:0(?:x|X))?([0-9a-fA-F]{2})+$";
+    }
 }
